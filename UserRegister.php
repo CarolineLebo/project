73a38069032188122727b4e90f3bea32666fbdb6
CarolineLebo <caroline.molele@gmail.com>
2014-05-09 16:13:13 +0200

    Update UserRegister.php

--- UserRegister.php
+++ UserRegister.php
@@ -105,6 +105,48 @@
 		</footer>
           </form>
 </div>	
+
+<?php 
+
+$firstName=strtoupper($_POST['firstName']);
+$surname=strtoupper($_POST['surname']);
+$cell=$_POST['cell'];
+$email=$_POST['email'];
+$co=$_POST['country'];
+$city=$_POST['city'];
+$add=$_POST['address'];
+$pass=$_POST['password'];
+$pass1=$_POST['password1'];
+
+$conn=mysql_connect('localhost','projects','bags');
+
+	if(!$conn)
+	{
+	echo 'Could not connect to the database';	
+	exit;
+	}
+	
+	$db = mysql_SELECT_db('c&tbags_db');
+
+	if(!$db)
+{
+	echo 'Database selection failed';	
+	exit;
+}
+$insert="insert into customer values(null, '$firstName','$surname','$pass','$email','$co','$city','$add','$pass')";
+  echo $query;
+ 
+$results = mysql_query($insert);
+
+if(!$results)
+{
+	echo 'submission failed';	
+	exit;
+}
+
+echo mysql_affected_rows($conn)."user added in";
+ ?>
+
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-1.11.0.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
